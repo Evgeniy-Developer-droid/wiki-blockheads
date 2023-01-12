2a83e9697af204c68edb279fb3c727b187ec958f
Evgeniy-Developer-droid <evgeniy.python.developer@gmail.com>
2023-01-12 16:37:47 +0200

    static imgs

--- WIKISite/settings.py
+++ WIKISite/settings.py
@@ -122,10 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles'
-]
+if DEBUG:
+    STATIC_ROOT = BASE_DIR / 'static'
+else:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static'
+    ]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
